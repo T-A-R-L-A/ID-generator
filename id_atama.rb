7cdf395eddb5f71b27ca60de6febe44e78017bcd
Şebnem Manav <sebnemmanav@gmail.com>
2015-06-26 13:40:57 +0300

    changed swich-case structer, code is running

--- id_atama.rb
+++ id_atama.rb
@@ -1,6 +1,9 @@
 # ID-router project
 # Alan ve Stajyer-Çalışan seçme işlemi
 # with Switch Statement
+count1 = 101
+count2 = 999
+
 while 1 do
   puts "Please select your subproject: "
   subprojects = ['Management','Beam Physics','Control','Electric','Electronics','IT','Mechanical Systems','RF','Vacuum','Radiation Safety']
@@ -13,57 +16,54 @@ while 1 do
 
   case selection1
   when '1'
-    $id1 = '01'
+    id1 = '01'
   when '2'
-    $id1 = '02'
+    id1 = '02'
   when '3'
-    $id1 = '03'
+    id1 = '03'
   when '4'
-    $id1 = '04'
+    id1 = '04'
   when '5'
-    $id1 = '05'
+    id1 = '05'
   when '6'
-    $id1 = '06'
+    id1 = '06'
   when '7'
-    $id1 = '07'
+    id1 = '07'
   when '8'
-    $id1 = '08'
+    id1 = '08'
   when '9'
-    $id1 = '09'
+    id1 = '09'
   when '10'
-    $id1 = '10'
+    id1 = '10'
   else
-    $id1 = false
+    id1 = false
   end
 
   puts "Are you employee or intern?"
   print " 1-Employee\n 2-Intern\n"
   selection2 = gets.chomp
 
-  count1 = 101
-  count2 = 999
-
-  case selection2
-  when '1'
-    $id2 = count1
-  #  count1++
-  when '2'
-    $id2 = count2
-  #  count2--
+# Switch statement converted to if-else statement and counter worked
+  if selection2 == '1' then
+    id2 = count1
+    count1 += 1
+  elsif selection2 == '2' then
+    id2 = count2
+    count2 -= 1
   else
-    $id2 = false
+    id2 = false
   end
   # check system for wrong choices
-  if ( $id1 == false || $id2 == false ) then
+  if ( id1 == false || id2 == false ) then
     puts "Your choice is wrong!"
-    $id = 'No!'
+    id = 'No!'
   else
-    $id = $id1 + $id2.to_s
+    id = id1 + id2.to_s
   end
 
-  puts "Your ID: #$id"
+  puts "Your ID: #{id}"
 
-  if $id != 'No!' then   # if choices are true , continue; else back to top
+  if id != 'No!' then   # if choices are true , continue; else back to top
     #Did or will it continue or end of the cycle that determines the code block
     puts "Do you want to add a new one?(Y/N)"
     selection3 = gets.chomp
@@ -79,59 +79,62 @@ while 1 do
 
       case selection1
       when '1'
-        $id1 = '01'
+        id1 = '01'
       when '2'
-        $id1 = '02'
+        id1 = '02'
       when '3'
-        $id1 = '03'
+        id1 = '03'
       when '4'
-        $id1 = '04'
+        id1 = '04'
       when '5'
-        $id1 = '05'
+        id1 = '05'
       when '6'
-        $id1 = '06'
+        id1 = '06'
       when '7'
-        $id1 = '07'
+        id1 = '07'
       when '8'
-        $id1 = '08'
+        id1 = '08'
       when '9'
-        $id1 = '09'
+        id1 = '09'
       when '10'
-        $id1 = '10'
+        id1 = '10'
       else
-        $id1 = false
+        id1 = false
       end
 
       puts "Are you employee or intern?"
       print " 1-Employee\n 2-Intern\n"
       selection2 = gets.chomp
 
-      count1 = 101
-      count2 = 999
-
-      case selection2
-      when '1'
-        $id2 = count1
-      #  count1++
-      when '2'
-        $id2 = count2
-      #  count2--
+# Switch statement converted to if-else statement and counter worked
+      if selection2 == '1' then
+        id2 = count1
+        count1 += 1
+      elsif selection2 == '2' then
+        id2 = count2
+        count2 -= 1
       else
-        $id2 = false
+        id2 = false
       end
       # check system for wrong choices
-      if ( $id1 == false || $id2 == false ) then
+      if ( id1 == false || id2 == false ) then
         puts "Your choice is wrong!"
-        $id = 'No!'
+        id = 'No!'
       else
-        $id = $id1 + $id2.to_s
+        id = id1 + id2.to_s
       end
 
-      puts "Your ID: #$id"
+      puts "Your ID: #{id}"
     elsif (selection3 == 'N' || selection3 == 'n')
       break
     else
       puts "Your choice is wrong!"
     end
+  end
+
+  puts "Do you want to add a new one?(Y/N)"
+  selection3 = gets.chomp
+  if (selection3 == 'N' || selection3 == 'n')
+    break
   end
 end
