58d7cce81b903c48c000c79ad174394dbc41ccf6
Şebnem Manav <sebnemmanav@gmail.com>
2015-07-03 14:47:38 +0300

    Modification : Added Modules

---
+++ id-atama.rb
@@ -0,0 +1,91 @@
+# ID-router project
+# Modification : Added Modules
+
+# Show subproject of TARLA
+def show_subproject
+  puts "Please select your subproject: "
+  subprojects = ['Management','Beam Physics','Control','Electric','Electronics','IT','Mechanical Systems','RF','Vacuum','Radiation Safety']
+  i = 1
+  subprojects.each do |value|
+    puts "#{i}- #{value}"
+    i += 1
+  end
+end
+# Select subpojects
+def select_subproject(selection)
+  case selection
+  when '1'
+    '1'
+  when '2'
+    '2'
+  when '3'
+    '3'
+  when '4'
+    '4'
+  when '5'
+    '5'
+  when '6'
+    '6'
+  when '7'
+    '7'
+  when '8'
+    '8'
+  when '9'
+    '9'
+  when '10'
+    '10'
+  else
+    false
+  end
+end
+# Show position at TARLA
+def show_position
+  puts "Are you employee or intern?"
+  print " 1-Employee\n 2-Intern\n"
+end
+$count1 = 100
+$count2 = 1000
+# Select position
+def select_position(selection)
+  if selection == '1' then
+    $count1 += 1
+  elsif selection == '2' then
+    $count2 -= 1
+  else
+    return false
+  end
+end
+# selection control
+def check_selection(id_a, id_b)
+  if ( id_a == false || id_b == false ) then
+    puts "Your choice is wrong! Your ID: No!"
+  else
+    puts "Your ID:  ",id_a + id_b.to_s
+  end
+end
+# Asks whether to continue
+def ask_continue
+  puts "Do you want to add a new one?(Y/N)"
+  answer = gets.chomp
+  if answer == 'y' || answer == 'Y'
+  elsif answer == 'n' || answer == 'N'
+    exit(1)
+  else
+    puts "Your choice is wrong!"
+  end
+end
+
+while 1 do
+# Main modules
+  show_subproject
+  selection1 = gets.chomp
+  id1 = select_subproject(selection1) # id1 => The first two digits of ID
+  #puts "id1: #{id1}"
+  show_position
+  selection2 = gets.chomp
+  id2 = select_position(selection2) # id2 => The last three digits of ID
+  #puts "id2: #{id2}"
+  check_selection(id1 ,id2)
+
+  ask_continue
+end
